f907314b6071fd19f94cc206ca934c18af846b39
Virit <viritmail@gmail.com>
2022-09-26 20:42:43 +0800

    引入包添加js后缀

--- utils/build.js
+++ utils/build.js
@@ -1,14 +1,14 @@
 import webpack from 'webpack'
-import config from '../webpack.config'
+import config from '../webpack.config.js'
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
-process.env.ASSET_PATH = '/';
+process.env.BABEL_ENV = 'production'
+process.env.NODE_ENV = 'production'
+process.env.ASSET_PATH = '/'
 
-delete config.chromeExtensionBoilerplate;
+delete config.chromeExtensionBoilerplate
 
-config.mode = 'production';
+config.mode = 'production'
 
 webpack(config, function (err) {
   if (err) throw err;
